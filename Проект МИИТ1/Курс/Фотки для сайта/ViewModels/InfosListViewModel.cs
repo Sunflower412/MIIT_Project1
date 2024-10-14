0895cb669fe8c19496027cdfad568ac83c1f7cbf
Sunflower412 <140610058+Sunflower412@users.noreply.github.com>
2024-10-15 00:44:05 +0300

    Add files via upload

---
+++ Проект МИИТ1/Курс/Фотки для сайта/ViewModels/InfosListViewModel.cs
@@ -0,0 +1,16 @@
+﻿using WebCourse.Data.models;
+
+namespace WebCourse.ViewModels
+{
+	public class InfosListViewModel
+	{
+		internal IEnumerable<Info> allInfos;
+		internal string currCategory;
+
+		public IEnumerable<Info> getAllInfos {  get; set; }
+		
+		public string infoCategory { get; set; }
+
+		
+	}
+}
