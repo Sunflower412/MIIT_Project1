a1fd2e51d65c416cbaf0ab6edb12801e59a572b8
Sunflower412 <140610058+Sunflower412@users.noreply.github.com>
2024-10-15 00:45:39 +0300

    Add files via upload

---
+++ Проект МИИТ1/Курс/Фотки для сайта/Startup.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using WebCourse.Data.Interfaces;
+using WebCourse.Data.mocks;
+
+namespace WebCourse
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<IAllAbillities, mockAbillities>();
+            services.AddTransient<IInformationCategory, mockCategory>();
+            services.AddMvc();
+            services.AddMvc(options => options.EnableEndpointRouting = false);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostEnvironment env)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+
+
+		    if (env.IsDevelopment())
+				{
+					app.UseDeveloperExceptionPage();
+				}
+			if (env.IsProduction())
+			{
+				app.Run(async (context) =>
+				{
+					await context.Response.WriteAsync("Production! ACY CC");
+
+				});
+			}
+
+		}
+    }
+}
